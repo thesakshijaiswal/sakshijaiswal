470ac6567c6f36dafb599889082883c58a9bcb4c
Sakshi Jaiswal <sakshijaiswal0211@gmail.com>
2024-05-25 19:04:19 +0530

    feat: create function isValidEmail

--- index.js
+++ index.js
@@ -18,13 +18,34 @@ const setError = (element, message) => {
     inputControl.classList.remove('success');
 }
 
+const setSuccess = () => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 const validateInputs = () => {
     const emailValue = email.value.trim();
     const dateValue = date.value.trim();
     const passwordValue = password.value.trim();
 
     if(emailValue === ''){
+        setError(email, 'Please add valid email address');
+    }else{
+        setSuccess(email);
+    }
 
+    if(dateValue === ''){
+        setError(date, 'Please enter valid date');
+    }else{
+        setSuccess(date);
     }
 
 };
